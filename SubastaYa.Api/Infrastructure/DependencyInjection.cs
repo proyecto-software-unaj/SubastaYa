77ab9ce5d61f21352a94348899ded1c06d60ef05
Cristian <71187504+Cristian-E-Ponce@users.noreply.github.com>
2026-09-18 20:18:23 -0300

    feat: se realiza migracion y creacion en sql server

---
+++ SubastaYa.Api/Infrastructure/DependencyInjection.cs
@@ -0,0 +1,22 @@
+﻿using Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(
+        this IServiceCollection services,
+        IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(connectionString));
+
+            return services;
+        }
+    }
+}
