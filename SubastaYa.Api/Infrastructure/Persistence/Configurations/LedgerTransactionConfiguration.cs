1ec2d06bc200cd794a2b3ee954c2b21dd3d664fb
Cristian <71187504+Cristian-E-Ponce@users.noreply.github.com>
2026-09-18 10:15:23 -0300

    feat: agregar DbContext y configuraciones de entidades

---
+++ SubastaYa.Api/Infrastructure/Persistence/Configurations/LedgerTransactionConfiguration.cs
@@ -0,0 +1,36 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+
+namespace Infrastructure.Persistence.Configurations
+{
+    public class LedgerTransactionConfiguration : IEntityTypeConfiguration<LedgerTransaction>
+    {
+        public void Configure(EntityTypeBuilder<LedgerTransaction> builder)
+        {
+            builder.HasKey(t => t.Id);
+
+            builder.Property(t => t.Amount)
+                   .HasColumnType("decimal(18,2)")
+                   .IsRequired();
+
+            builder.Property(t => t.Type)
+                   .HasConversion<string>()
+                   .HasMaxLength(20)
+                   .IsRequired();
+
+            builder.Property(t => t.CreatedAt)
+                   .IsRequired();
+
+            builder.HasIndex(t => new { t.WalletId, t.CreatedAt });
+
+            builder.HasOne(t => t.Auction)
+                   .WithMany(a => a.LedgerTransactions)
+                   .HasForeignKey(t => t.AuctionId)
+                   .OnDelete(DeleteBehavior.Restrict);
+
+        }
+    }
+}
---
+++ SubastaYa.Api/Infrastructure/Persistence/Configurations/WalletConfiguration.cs
@@ -0,0 +1,35 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+
+namespace Infrastructure.Persistence.Configurations
+{
+    public class WalletConfiguration : IEntityTypeConfiguration<Wallet>
+    {
+        public void Configure(EntityTypeBuilder<Wallet> builder)
+        {
+            builder.HasKey(w => w.Id);
+
+            builder.HasIndex(w => w.UserId).IsUnique();
+
+            builder.Property(w => w.TotalBalance)
+                   .HasColumnType("decimal(18,2)")
+                   .IsRequired();
+
+            builder.Property(w => w.HeldBalance)
+                   .HasColumnType("decimal(18,2)")
+                   .IsRequired();
+
+            builder.Ignore(w => w.AvailableBalance);
+
+            builder.Property(w => w.RowVersion).IsRowVersion();
+
+            builder.HasMany(w => w.LedgerTransactions)
+                   .WithOne(t => t.Wallet)
+                   .HasForeignKey(t => t.WalletId)
+                   .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
