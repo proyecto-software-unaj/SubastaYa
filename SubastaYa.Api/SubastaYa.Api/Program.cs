78462aa76f353d1d8ef870abdddb99b036a27e47
Cristian <71187504+Cristian-E-Ponce@users.noreply.github.com>
2026-09-19 17:12:51 -0300

    feat: documentacion backend y pruebas postman

--- SubastaYa.Api/SubastaYa.Api/Program.cs
+++ SubastaYa.Api/SubastaYa.Api/Program.cs
@@ -6,7 +6,7 @@ using SubastaYa.Api.Hubs;
 using SubastaYa.Api.Workers;
 
 var builder = WebApplication.CreateBuilder(args);
-
+const string CorsPolicy = "FrontendPolicy";
 // Add services to the container.
 
 builder.Services.AddControllers();
@@ -14,6 +14,23 @@ builder.Services.AddInfrastructure(builder.Configuration);
 builder.Services.AddHostedService<AuctionClosingWorker>();
 builder.Services.AddSignalR();
 builder.Services.AddScoped<IAuctionNotifier, SignalRAuctionNotifier>();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(CorsPolicy, policy =>
+    {
+        policy
+            .WithOrigins(
+                "http://localhost:5173",   // Vite (React/Vue)
+                "http://localhost:3000",   // Create React App / Next
+                "http://127.0.0.1:5500",   // Live Server (VS Code)
+                "http://localhost:5500"
+            )
+            .AllowAnyHeader()
+            .AllowAnyMethod()
+            .AllowCredentials();
+    });
+});
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(options =>
@@ -37,9 +54,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();    
 }
 
+
 app.UseHttpsRedirection();
+app.UseCors(CorsPolicy);
 app.UseAuthorization();
 app.MapControllers();
-app.MapHub<AuctionHub>("/hubs/auctions");
+app.MapHub<AuctionHub>("/hubs/auctions").RequireCors(CorsPolicy);
 
 app.Run();
